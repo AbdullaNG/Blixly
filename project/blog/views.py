743c658a24f0837c33f0182720619aa2f95fc5b2
Abdulla <abdullacoder17@gmail.com>
2025-10-19 17:27:30 +0500

    Created new templates

--- project/blog/views.py
+++ project/blog/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
 
 
 def home(request):
 	context = {'posts': Post.objects.all()}
 	return render(request, 'blog/index.html', context)
+
+class PostListView(ListView):
+	model = Post
+	template_name ='blog/index.html'
+	context_object_name = 'posts'
+	ordering = ['-date_posted']
 
 def about(request):
 	return render(request, 'blog/about.html')
